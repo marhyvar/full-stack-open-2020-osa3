7554767239f397a08ca2bab234a5b1ae00d794de
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-07-20 14:49:24 +0300

    3.15 puhelinluettelo ja tietokanta step3

--- index.js
+++ index.js
@@ -73,9 +73,13 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+    /*const id = Number(req.params.id)
     persons = persons.filter(p => p.id !== id)
-    res.status(204).end()
+    res.status(204).end()*/
 })
 
 app.post('/api/persons', (req, res) => {   
